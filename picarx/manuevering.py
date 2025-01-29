930eaafa89616b06601bf63bce7d8463c6d37cbb
tracyyang816 <yyang5@fandm.edu>
2025-01-29 15:09:12 -0800

    angle

--- picarx/manuevering.py
+++ picarx/manuevering.py
@@ -128,7 +128,7 @@ def three_point_parking(px, dir):
 
 def line_following(px):
     sensor = Sensor() # px.get_grayscale_data()
-    interpretor = Interpretor(500, "darker") # might adjust these value later
+    interpretor = Interpretor(100, "darker") # might adjust these value later
     controller = Controller(px, 30)
 
     try:
