5ef03fa48a883d2d9974b87b4b2dff23115c928a
tracyyang816 <yyang5@fandm.edu>
2025-02-11 12:54:06 -0800

    threads

--- picarx/threads.py
+++ picarx/threads.py
@@ -16,9 +16,10 @@ sensor = Sensor()
 controller = Controller(px, 30)
 interpretor = Interpretor(100, "darker")
 
-def sensor_function(adc_bus, delay): # producer 
+def sensor_function(adc_bus, delay): # producer
     
-    while True:
+    while not shutdown_event.is_set():
+        print("sensor reading")
         adc_val = sensor.read_sensors()
         adc_bus.write(adc_val)
         time.sleep(delay)
@@ -27,7 +28,8 @@ def sensor_function(adc_bus, delay): # producer
 
 def controller_function(pos_bus, delay): # consumer 
     
-    while True:
+    while not shutdown_event.is_set():
+        print("controller driving")
         pos = pos_bus.read()
         controller.drive(pos)
         time.sleep(delay)
@@ -36,7 +38,8 @@ def controller_function(pos_bus, delay): # consumer
 
 def interpreter_function(adc_bus, pos_bus, delay): # consumer_producer
     
-    while True:
+    while not shutdown_event.is_set():
+        print("interpreter read and write")
         adc_val = adc_bus.read()
         pos = interpretor.process(adc_val, pos_bus.read())
         pos_bus.write(pos)
@@ -55,11 +58,13 @@ def handle_exception(future):
         print(f"Exception in worker thread: {exception}")
 
 
+'''
 # Define robot task
 def robot_task(i):
     print("Starting robot task", i)
     while not shutdown_event.is_set():
         # Run some robot task...
+
         print("Running robot task", i)
         time.sleep(1)
         # Print shut down message
@@ -68,7 +73,7 @@ def robot_task(i):
     if i == 1:
         raise Exception("Robot task 1 raised an exception")
 
-
+'''
 
 
 
@@ -79,15 +84,15 @@ if __name__ == "__main__":
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
         eSensor = executor.submit(sensor_function, sensor_values_bus, 1)
         eInterpreter = executor.submit(interpreter_function,sensor_values_bus, interpreter_bus,1)
-        eController = executor.submit(interpreter_bus, 1)
+        eController = executor.submit(controller_function, interpreter_bus, 1)
 
     
-
+    robot_tasks = [eSensor, eInterpreter, eController]
     futures = []
     with ThreadPoolExecutor(max_workers=3) as executor:
         for i in range(3):
             # Spawn task threads
-            future = executor.submit(robot_task, i)
+            future = robot_tasks[i]
             # Add exception call back
             future.add_done_callback(handle_exception)
             futures.append(future)
