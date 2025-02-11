c2b5a77deda0467d493ddc346f8de2c52ef87b01
tracyyang816 <yyang5@fandm.edu>
2025-02-11 13:11:27 -0800

    sensor

--- picarx/sensor.py
+++ picarx/sensor.py
@@ -1,11 +1,15 @@
+from robot_hat import ADC
 
-try:
-    from robot_hat import ADC
-
-except ModuleNotFoundError:
-    from sim_robot_hat import ADC
 
 class Sensor():
+    def __init__(self, adc_channels=['A0', 'A1', 'A2']):
+        self.adc_channels = adc_channels
+        self.adc = ADC()  # Single ADC instance
+    
+    def read_sensors(self):
+        return [self.adc.read(chn) for chn in self.adc_channels]
+
+    '''
     def __init__(self, adc_channels = ['A0', 'A1', 'A2']):
         # self.adcs = adc_channels
         self.adcs = [ADC(chn) for chn in adc_channels]
@@ -13,3 +17,5 @@ class Sensor():
     
     def read_sensors(self):
         return [adc.read() for adc in self.adcs]
+
+    '''
