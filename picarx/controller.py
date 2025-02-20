c9045ebcd22dbd92a0b7153cc0d667db652120ee
tracyyang816 <yyang5@fandm.edu>
2025-02-19 21:32:26 -0800

    threads

--- picarx/controller.py
+++ picarx/controller.py
@@ -12,12 +12,12 @@ class Controller():
         if has_line == False:
             # then we need to go find the line
             print("no line")
-            angle = random.choice([-20, 20])
+            angle = random.choice([-30, 30])
 
             # position = float(position)
             # angle = position * self.scalar
             self.px.set_dir_servo_angle(angle)
-            self.px.forward(30)
+            self.px.forward(10)
             time.sleep(0.02)
             self.px.stop()
             
