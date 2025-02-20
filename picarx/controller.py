30ede8a361b557206dedc2e098ea9c680b810cbf
tracyyang816 <yyang5@fandm.edu>
2025-02-19 21:46:10 -0800

    threads

--- picarx/controller.py
+++ picarx/controller.py
@@ -6,11 +6,12 @@ class Controller():
         self.px = px
         self.scalar = scalar
         self.no_line = 0
-        self.angle = 0
     
     def drive(self, position, has_line):
         
         if has_line == False:
+
+            '''
             self.no_line += 1
             # then we need to go find the line
             print("no line")
@@ -25,10 +26,10 @@ class Controller():
                 self.angle = -self.angle
                 self.no_line = 0
 
-                
+            '''
 
-            self.px.set_dir_servo_angle(self.angle)
-            self.px.forward(30)
+            self.px.set_dir_servo_angle(0)
+            self.px.backward(30)
             time.sleep(0.2)
             self.px.stop()
             
