232fd87f93c8bdbf976c85b12ce72a83a5a8c133
tracyyang816 <yyang5@fandm.edu>
2025-02-27 19:07:49 -0800

    rr debug

--- picarx/sensor.py
+++ picarx/sensor.py
@@ -12,7 +12,7 @@ class Sensor():
 
     
     def read_sensors(self):
-        print(adc.read() for adc in self.adcs)
+        print([adc.read() for adc in self.adcs])
         return [adc.read() for adc in self.adcs]
 
 
